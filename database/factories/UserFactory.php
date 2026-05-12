c2b260cf566f904262fcf7d7b21d98f893d822a8
Angel Sanabria <as1945228@gmail.com>
2026-05-11 23:06:21 -0600

    feat: add role attribute to UserFactory and remove VeterinarianFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'phone' => fake()->phoneNumber(),
+            'role' => fake()->randomElement(['admin', 'veterinarian', 'receptionist']),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
